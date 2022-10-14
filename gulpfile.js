873ba979c993141e9cd8590d81e5b2c71f77a546
Jesse Freeman <jessefreeman@gmail.com>
2022-10-14 09:27:37 -0700

    Cleaning up

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 const gulp = require('gulp');
 const shell = require('gulp-shell')
 
-// Set the path to the .csproj file
-process.env.PROJECT = 'oop-adventure.csproj';
-process.env.OPTIONS = ' -p:SelfContained=false -p:PublishSingleFile=false';
-process.env.OUTPUT = ' -o ./Releases/';
-
-// The default task just build the game locally which you can launch and debug
-gulp.task('default', gulp.series([shell.task('dotnet build ' + process.env.PROJECT + process.env.OPTIONS + process.env.OUTPUT)]))
+gulp.task(
+    'default', 
+    gulp.series(
+        [shell.task('dotnet pack oop-adventure.csproj -o ./Releases/')]
+        )
+    );
